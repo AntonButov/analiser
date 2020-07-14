1c8b210ff00912dcf28b6ff04bb6d57877a7400b
Anton Butov <butov6101@gmail.com>
2020-07-14 18:36:01 +0300

    Все помещается.

--- app/src/main/java/pro/butovanton/analizer/Model.java
+++ app/src/main/java/pro/butovanton/analizer/Model.java
@@ -123,4 +123,30 @@ public class Model {
             }
         return result;
     }
+
+    public String[] findLeftUp(String findStr) {
+        String[] result = new String[2];
+        result[0] = "";
+        result[1] = "";
+        for (int i = 0; i < data.size(); i ++)
+            for (int x = 0; x < 4; x ++) {
+                if (i >= findStr.length() - 1 && x + findStr.length() <= 4 ) {
+                    int l;
+                    for ( l = 0; l < findStr.length(); l++)
+                        if (data.get(i - l).charAt(x + l) != findStr.charAt(l))
+                            break;
+                    if (l == findStr.length()) {
+                        for (l = 0; l < findStr.length(); l++ )
+                            if (i - l -1 >= 0)
+                                result[0] = result[0] + data.get(i - l - 1).charAt(x + l);
+                            else result[0] = result[0] + " ";
+                        for (l = 0; l < findStr.length(); l++ )
+                            if (i + 1 < data.size())
+                                result[1] = result[1] + data.get(i - l + 1).charAt(x + l);
+                        return result;
+                    }
+                }
+            }
+        return result;
+    }
 }
